282278e0ce616e823c83181c168dd26143caf70c
Donci31 <82709410+Donci31@users.noreply.github.com>
2025-11-27 17:50:19 +0100

    Pyright -> Pyrefly in vim and Removed rust

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,2 +1,23 @@
-vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_lsp = "pyrefly"
 vim.g.lazyvim_python_ruff = "ruff"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
+
+vim.o.shell = "nu"
