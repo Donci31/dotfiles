52da7f6bf49961a260f035e8210e68ded1e60072
Donci31 <82709410+Donci31@users.noreply.github.com>
2025-11-27 16:23:46 +0100

    Pyright -> Pyrefly

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,18 +3,8 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        pyright = {
+        pyrefly = {
           enabled = true,
-          settings = {
-            python = {
-              analysis = {
-                autoSearchPaths = true,
-                diagnosticMode = "workspace",
-                useLibraryCodeForTypes = true,
-                typeCheckingMode = "basic",
-              },
-            },
-          },
         },
         ruff = {
           enabled = true,
@@ -28,53 +18,22 @@ return {
       },
     },
   },
-
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-
-      vim.list_extend(opts.sources, {
-        nls.builtins.diagnostics.ruff.with({
-          extra_args = { "--extend-select", "I" },
-        }),
-        nls.builtins.formatting.ruff_format,
-      })
-
-      return opts
-    end,
-  },
-
   {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
-        "pyright",
+        "pyrefly",
         "ruff",
-        "ruff-lsp",
         "debugpy",
       },
     },
   },
-
   {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
       formatters_by_ft = {
         python = { "ruff_format", "ruff_fix" },
-      },
-    },
-  },
-
-  {
-    "mfussenegger/nvim-lint",
-    optional = true,
-    opts = {
-      linters_by_ft = {
-        python = { "ruff" },
       },
     },
   },
